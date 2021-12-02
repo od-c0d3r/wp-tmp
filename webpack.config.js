d2a3d29a5ca68ceaa52275a2eaa3ba379e40b453
od-c0d3r <omar_rashad@aol.com>
2021-12-02 12:22:25 +0200

    Repo: Setup local dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
         index: './src/index.js',
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack Template',
             template: './src/index.html',
         }),
     ],
