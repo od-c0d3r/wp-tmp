d17fbf9336af75d5cbbaddbab3d8a3ff6182ad86
Omar R <o.rashad2020@gmail.com>
2021-12-02 12:41:47 +0200

    Repo: Remote inline comments

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import './style.css';
 
 function component() {
     const element = document.createElement('div');
-
-    // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
     return element;
 }
 
